e6191dc4972199aba0adb9951673c676b3840baa
Dorsa Ershadifar <er.dorsa@gmail.com>
2022-11-27 20:34:44 -0500

    Updates

--- data.js
+++ data.js
@@ -1,28 +1,28 @@
 const characterData = {
     hero: {
         name: "Wizard",
-        avatar: "images/wizard.png",
+        avatar: "https://i.pinimg.com/736x/f5/19/45/f51945072c53cc86a676c339f3d2a574.jpg",
         health: 60,
         diceCount: 3,
         currentDiceScore: []
     },
     orc: {
         name: "Orc",
-        avatar: "images/orc.png",
+        avatar: "https://i.pinimg.com/736x/31/d4/f4/31d4f4fe850a119b3bbb0877929ebcce.jpg",
         health: 30,
         diceCount: 1,
         currentDiceScore: []
     },
     demon: {
         name: "Demon",
-        avatar: "images/demon.png",
+        avatar: "https://i.pinimg.com/originals/e9/d8/0e/e9d80e661fade34f1edff71168df9592.png",
         health: 25,
         diceCount: 2,
         currentDiceScore: []
     },
     goblin: {
         name: "Goblin",
-        avatar: "images/goblin.png",
+        avatar: "https://i.pinimg.com/originals/72/0b/28/720b281091e2474cae035cd71ef9b17e.png",
         health: 20,
         diceCount: 3,
         currentDiceScore: []
